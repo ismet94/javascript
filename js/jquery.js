b2b600bd0367b3e3c9985ae2639f89b6676c8745
ismet94 <ismetsamso94@gmail.com>
2019-03-19 13:04:03 +0200

    Updated lesson with Jquery

---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+// Jquery open source javascript library allows you to harness the power of Javascript to accomplish a myriad of awsome things on your webpage
+
+// Add delete or modify HTML elements within your page
+
+// Change the styles of elemtns on the page by modifying their associated CSS.
+
+// Send and receive data from a server via AJAX (asychronous Javascript and XML) So your page doesn't have to be reloaded after submitting a form.
+
+// And More?
+
+let listItems = document.querySelectorAll('li');
+let i;
+for (i = 0; i < listItems.length; i++) {
+    listItems
+}
